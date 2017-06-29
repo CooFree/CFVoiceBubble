6834d01ea2466a8b8e4c1b306145c19360bdecf0
Ken <coofree@foxmail.com>
2017-06-29 16:38:30 +0800

    Initial commit

---
+++ CFVoiceBubble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFVoiceBubble
+//
+//  Created by YF on 2017/6/29.
+//  Copyright © 2017年 CooFree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
