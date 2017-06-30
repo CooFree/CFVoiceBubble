31cb7dbe06ffad9316265db8bef1da19055be3e8
Ken <coofree@foxmail.com>
2017-06-30 09:28:36 +0800

    3

--- CFVoiceBubble/CFVoiceBubble/CFVoiceBubble.h
+++ CFVoiceBubble/CFVoiceBubble/CFVoiceBubble.h
@@ -8,6 +8,76 @@
 
 #import <UIKit/UIKit.h>
 
+@class CFVoiceBubble;
+
+
+/**
+ <#Description#>
+ */
+@protocol CFVoiceBubbleDelegate <NSObject>
+
+- (void)voiceBubbleDidStartPlaying:(CFVoiceBubble *)voiceBubble;
+
+@end
+
+
 @interface CFVoiceBubble : UIView
+
+/**
+ <#Description#>
+ */
+@property (assign, nonatomic) BOOL    isLoad;
+
+/**
+ <#Description#>
+ */
+@property (strong, nonatomic) NSString *URLStr;
+
+/**
+ <#Description#>
+ */
+@property (strong, nonatomic) NSURL *contentURL;
+
+/**
+ <#Description#>
+ */
+@property (strong, nonatomic) IBInspectable UIColor *waveColor;
+
+/**
+ <#Description#>
+ */
+@property (strong, nonatomic) IBInspectable UIColor *animatingWaveColor;
+
+/**
+ <#Description#>
+ */
+@property (strong, nonatomic) IBInspectable UIImage *bubbleImage;
+
+/**
+ <#Description#>
+ */
+@property (assign, nonatomic) IBInspectable BOOL    invert;
+
+/**
+ <#Description#>
+ */
+@property (assign, nonatomic) IBInspectable BOOL    exclusive;
+
+/**
+ <#Description#>
+ */
+@property (assign, nonatomic) IBInspectable BOOL    durationInsideBubble;
+
+@property (assign, nonatomic) IBOutlet id<CFVoiceBubbleDelegate> delegate;
+
+
+
+- (void)prepareToPlay;
+- (void)play;
+- (void)pause;
+- (void)stop;
+
+- (void)startAnimating;
+- (void)stopAnimating;
 
 @end
